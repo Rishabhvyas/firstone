af12e8015ec4b0ae624b1dfd7da4545cbf76c247
Rishabh Vyas <rishabhvyas47@gmail.com>
2017-09-25 18:11:52 +0530

    first day python code

---
+++ p.py
@@ -0,0 +1,10 @@
+id=["a@gmail.com","b@yahoo.com","c@gamil.com","d@yahoo.com"]
+
+count=0
+
+   if "gmail" in "a@gmail.com","b@yahoo.com","c@gamil.com","d@yahoo.com"]:
+       count++
+
+print(count)    
+       
+
